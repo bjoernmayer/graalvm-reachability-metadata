d62359b9245d774804c804f487a0225a7c5242ce
Björn Mayer <mayer.bjoern@web.de>
2026-04-15 10:01:56 +0200

    Prepare 1.0.1

--- graalvm-reachability-metadata/build.gradle.kts
+++ graalvm-reachability-metadata/build.gradle.kts
@@ -51,7 +51,7 @@ tasks {
 // endregion
 
 group = "io.github.bjoernmayer"
-version = "1.0.0"
+version = "1.0.1"
 
 gradlePlugin {
     website = "https://github.com/bjoernmayer/graalvm-reachability-metadata"
